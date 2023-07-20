c3ef14899c70e2a1f62f7a4f731b0c63830ccef4
LorenzoMegna24 <megnalorenzo0@gmail.com>
2023-07-20 15:50:35 +0200

    Offcanvas amenities

--- src/pages/ApartmentList.vue
+++ src/pages/ApartmentList.vue
@@ -24,6 +24,30 @@
 
 <template>
   <div class="container mt-5">
+    <!-- Offcanvas amenities -->
+    <a class="btn btn-primary" data-bs-toggle="offcanvas" href="#offcanvasExample" role="button" aria-controls="offcanvasExample">
+      Link with href
+    </a>
+    <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
+      <div class="offcanvas-header">
+        <h5 class="offcanvas-title" id="offcanvasExampleLabel">Filtri</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+      </div>
+      <div class="offcanvas-body">
+        <div>
+          <h2>Servizi</h2>
+        </div>
+        <div class=" mt-3">
+          <div class="form-check">
+            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+            <label class="form-check-label" for="flexCheckDefault">
+              Default checkbox
+            </label>
+          </div>
+        </div>
+      </div>
+    </div> 
+    <!-- Offcanvas amenities -->
     <div class="row justify-content-around pt-5">
       <div class="col-3 m-2" v-for="(elem,index) in apartments" :key="index">
         <div class="card">
