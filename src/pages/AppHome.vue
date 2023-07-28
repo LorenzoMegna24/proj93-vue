91a0dff024667ea4e345b5d90ce71973ec0dfeaa
alecasentini <ale.casentini@gmail.com>
2023-07-28 11:22:44 +0200

    Aggiunto scroll home

--- src/pages/AppHome.vue
+++ src/pages/AppHome.vue
@@ -26,6 +26,7 @@ export default {
     searchApartments() {
       this.getApartments(1);
       this.searched = true;
+      this.$refs.results.scrollIntoView({ behavior: 'smooth' });
     },
     getAddressSuggestions() {
       if (this.location) {
@@ -99,65 +100,66 @@ export default {
         <div class="cont-ricerca p-3">
 
           <h2>Ricerca una località</h2>
-          
-           <!-- barra di ricerca e bottone -->
+
+          <!-- barra di ricerca e bottone -->
 
           <div class="d-flex align-items-center">
 
-            <input class="form-control" type="text" v-model="location" autocomplete="off" placeholder="Inserisci indirizzo o città"
-            @input="getAddressSuggestions">
+            <input class="form-control" type="text" v-model="location" autocomplete="off"
+              placeholder="Inserisci indirizzo o città" @input="getAddressSuggestions">
 
             <button @click="searchApartments" class="btn btn-primary ms-3">Cerca</button>
-            
+
           </div>
 
           <!-- lista degli indirizzi consigliati -->
           <ul class="list-group mt-2" v-if="addressSuggestions.length > 0">
             <li class="list-group-item list-group-item-action lista-indirizzi" style="cursor: pointer;"
-            v-for="(address, index) in addressSuggestions" :key="index" @click="selectAddress(address)">
+              v-for="(address, index) in addressSuggestions" :key="index" @click="selectAddress(address)">
               {{ address }}
             </li>
           </ul>
 
-          <div v-if="freeformAddress" class="my-3">
-            Risultati per {{ freeformAddress }} nel raggio di {{ selectedRadius }}Km:
-          </div>
+
         </div>
-        
+
       </div>
 
     </div>
 
-      <div class="my-3 container-card">
+    <div ref="results" class="my-3 container-card">
 
 
-        <div class="row justify-content-around pt-5">
+      <div class="row justify-content-around pt-5">
 
+        <div v-if="freeformAddress" class="my-3">
+          Risultati per {{ freeformAddress }} nel raggio di {{ selectedRadius }}Km:
+        </div>
 
-          <div v-if="searched && apartments.length === 0" class="text-center my-3">
-            Non ci sono appartamenti nella località selezionata
-          </div>
+        <div v-if="searched && apartments.length === 0" class="text-center my-3">
+          Non ci sono appartamenti nella località selezionata
+        </div>
 
-          <div class="col-lg-4 col-md-6 col-sm-12 my-3" v-for="(elem, index) in apartments" :key="index">
-            <div class="card single-card border-0">
-              <img class="card-img-top rounded-3" :src="`${baseUrl}/storage/${elem.image}`" alt="Title">
-              <div class="card-body">
-                <RouterLink class="text-decoration-none" :to="{ name: 'apartment', params: { slug: elem.slug } }">
-                  <h5>{{ elem.title }}</h5>
-                </RouterLink>
-                <p class="card-text">{{ elem.address }}</p>
-                <p>Stanze: {{ elem.room }}</p>
-                <p>Letti: {{ elem.bed }}</p>
-                <p class="mb-1">Servizi:</p>
-                <p class="d-flex flex-wrap">
-                  <span v-for="amenity in elem.amenities" :key="amenity.id">
-                    <img class="me-2" :src="`${baseUrl}/storage/${amenity.image}`" :alt="amenity.name" style="height: 20px">
-                  </span>
-                </p>
-              </div>
+        <div class="col-lg-4 col-md-6 col-sm-12 my-3" v-for="(elem, index) in apartments" :key="index">
+          <div class="card single-card border-0">
+            <img class="card-img-top rounded-3" :src="`${baseUrl}/storage/${elem.image}`" alt="Title">
+            <div class="card-body">
+              <RouterLink class="text-decoration-none" :to="{ name: 'apartment', params: { slug: elem.slug } }">
+                <h5>{{ elem.title }}</h5>
+              </RouterLink>
+              <p class="card-text">{{ elem.address }}</p>
+              <p>Stanze: {{ elem.room }}</p>
+              <p>Letti: {{ elem.bed }}</p>
+              <p class="mb-1">Servizi:</p>
+              <p class="d-flex flex-wrap">
+                <span v-for="amenity in elem.amenities" :key="amenity.id">
+                  <img class="me-2" :src="`${baseUrl}/storage/${amenity.image}`" :alt="amenity.name" style="height: 20px">
+                </span>
+              </p>
             </div>
-
           </div>
+
+        </div>
       </div>
 
       <nav v-if="apartments.length > 0" aria-label="Page navigation">
@@ -181,7 +183,7 @@ export default {
 
     <div>
       <SponsorComp />
-     </div>
+    </div>
 
   </section>
 </template>
@@ -190,59 +192,60 @@ export default {
 section {
   background-color: rgb(204, 228, 253);
 
-  .welcome{
-      background-image: url("/img/frames-for-your-heart-2d4lAQAlbDA-unsplash.jpg");
-      background-position: center;
-      background-size:100vw;
-      background-repeat: no-repeat;
-      height: 90vh;
+  .welcome {
+    background-image: url("/img/frames-for-your-heart-2d4lAQAlbDA-unsplash.jpg");
+    background-position: center;
+    background-size: 100vw;
+    background-repeat: no-repeat;
+    height: 90vh;
 
-      .cont-ricerca{
-        background-color: rgb(204, 228, 253, 0.7);
-        border-radius: 25px;
-        width: fit-content;
+    .cont-ricerca {
+      background-color: rgb(204, 228, 253, 0.7);
+      border-radius: 25px;
+      width: fit-content;
 
 
-        input{
-          width: 30rem;
-          height: 3rem;
-        }
-
-        .lista-indirizzi{
-          width: 30rem;
-          max-height: 5rem;
-        }
+      input {
+        width: 30rem;
+        height: 3rem;
       }
 
-    }
-
-    .container-card{
-      margin-inline: 1%;
-
-      .single-card{
-        height: 34rem;
-        background-color: rgba($color: #FFFFFF, $alpha: 0.6);
-        
-        h5 {
-          color: #2382F7;
-        }
-        
-        .card-img-top {
-          height: 18rem;
-        }
-        
-        .card-text {
-          font-style: oblique;
-        }
-        
+      .lista-indirizzi {
+        width: 30rem;
+        max-height: 5rem;
       }
     }
-    
-    p {
-      margin-bottom: 0.5rem;
-    }
-    .single-card:hover img {
-      filter: brightness(0.7);
+
+  }
+
+  .container-card {
+    margin-inline: 1%;
+
+    .single-card {
+      height: 34rem;
+      background-color: rgba($color: #FFFFFF, $alpha: 0.6);
+
+      h5 {
+        color: #2382F7;
+      }
+
+      .card-img-top {
+        height: 18rem;
+      }
+
+      .card-text {
+        font-style: oblique;
+      }
+
     }
   }
-  </style>
+
+  p {
+    margin-bottom: 0.5rem;
+  }
+
+  .single-card:hover img {
+    filter: brightness(0.7);
+  }
+}
+</style>
